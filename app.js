66a4f73a90c252dfc741cc72de25865430296e3b
Jimmy Vu <JMV3211@gmail.com>
2025-09-07 14:26:00 -0700

    first route built

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello Squirrel');
+})
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+})
